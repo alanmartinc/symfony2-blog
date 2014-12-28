268103811fd35c0afdf9376f37831e9ce66a1b44
victorobs <victorobsan@gmail.com>
2014-12-28 15:09:49 +0100

    add post

--- src/Web/BlogBundle/Controller/PostsController.php
+++ src/Web/BlogBundle/Controller/PostsController.php
@@ -4,7 +4,8 @@ namespace Web\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Web\BlogBundle\Entity\Users;
-use Web\BlogBundle\Form\Type\RegistroType;
+use Web\BlogBundle\Entity\Posts;
+use Web\BlogBundle\Form\Type\PostsType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Security\Core\SecurityContext;
@@ -21,7 +22,7 @@ class PostsController extends Controller {
 
     public function indexAction(Request $request) {
         //Entity Manager
-        $em = $this->getDoctrine()->getEntityManager();
+        $em = $this->getDoctrine()->getManager();
         $categoryRepository=$em->getRepository("WebBlogBundle:Categories");
         $categories=$categoryRepository->findAll();
         
@@ -31,7 +32,76 @@ class PostsController extends Controller {
         ));
     }
     
-    
-  
+    public function newAction(Request $request){
+        //Entity Manager
+        $em = $this->getDoctrine()->getManager();
+        
+        //Categorias
+        $categoryRepository=$em->getRepository("WebBlogBundle:Categories");
+        $categories=$categoryRepository->findAll();
+        
+        $categories_array=array();
+        foreach($categories as $category){
+            $categories_array[$category->getId()]=$category->getName();
+        }
+        
+        
+        //instanciamos la entidad Posts
+        $post = new Posts();
 
+        //Creamos el formulario, asociado a la entidad
+        $form = $this->createForm(new PostsType(), $post);
+        
+        $form->add('category', 'choice', array(
+                "label" => "Categorias: ",
+                "attr" => array('class' => 'form-control'),
+                'choices' => $categories_array
+        ));
+
+        //utilizamos el manejador de peticiones
+        $form->handleRequest($request);
+
+        //Si el formulario ha sido enviado
+        if ($form->isSubmitted()) {
+           
+            //Metemos en variables los datos que llegan desde el formulario
+            $title = $form->get('title')->getData();
+            $description = $form->get('description')->getData();
+            $image = $form->get('image')->getData();
+            $content = $form->get('content')->getData();
+            $category_id = $form->get('category')->getData();
+            $category=$categoryRepository->find($category_id);
+            $user=$this->get('security.context')->getToken()->getUser();
+            
+            //Llamamos a los metodos set de la entidad y les metemos los valores del formulario
+            $post->setTitle($title);
+            $post->setDescription($description);
+            $post->setImage($image);
+            $post->setContent($content);
+            $post->setCategory($category);
+            $post->setUser($user);
+        }else{
+            
+        }
+        
+        //Si el formulario es valido tras aplicar la validacion de la entidad
+        if ($form->isValid()) {
+            $persist = $em->persist($post);
+            $flush = $em->flush();
+            
+            $this->session->getFlashBag()->add('new', '¡Enhorabuena! Has creado un nuevo post correctamente');
+            return $this->redirect($this->generateURL('home'));
+        }else{
+            if ($form->isSubmitted()) {
+                $this->session->getFlashBag()->add('new', 'Rellena correctamente el formulario');
+            }
+        }
+        
+        return $this->render('WebBlogBundle:Posts:new.html.twig', array(
+            "new_post_form" => $form->createView(),
+            "categories"=>$categories
+        ));
+    }
+    
+    
 }
---
+++ src/Web/BlogBundle/Form/Type/PostsType.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Web\BlogBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class PostsType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title','text', array("label" => "Titulo: ",
+                    "required" => false,
+                    "attr" => array('class' => 'form-control')))
+                
+            ->add('description','textarea', array("label" => "Descripción: ",
+                    "required" => false,
+                    "attr" => array('class' => 'form-control')))
+                
+            ->add('image','file', array("label" => "Imagen: ",
+                    "required" => false,
+                    "attr" => array('class' => '')))
+                
+            ->add('content','textarea', array("label" => "Contenido: ",
+                    "required" => false,
+                    "attr" => array('class' => 'form-control')))
+                
+            ->add('Guardar', 'submit', array("attr" => array('class' => 'btn btn-success')));
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Web\BlogBundle\Entity\Posts'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'web_blogbundle_posts';
+    }
+}
